9c45875f29cbea8b3e4ab7674db0115279d426b1
Riza Fahmi <rizafahmi@gmail.com>
2022-04-30 23:36:28 +0700

    Finish add Counter app for basic react testing

---
+++ 3-counter/src/Counter.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Counter from './Counter';
+
+test('renders learn react link', () => {
+  render(<Counter />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
