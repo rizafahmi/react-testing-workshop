9a311b7cec514ea935b93aa7be3d848ed146b968
Riza Fahmi <rizafahmi@gmail.com>
2022-05-01 11:05:48 +0700

    Start testing Counter app

--- 3-counter/src/Counter.test.js
+++ 3-counter/src/Counter.test.js
@@ -1,8 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render } from 'react-dom';
 import Counter from './Counter';
 
-test('renders learn react link', () => {
-  render(<Counter />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('render counter', function () {
+  // Arrange
+  const div = document.createElement('div');
+  document.body.append(div);
+  render(<Counter />, div);
+
+  // Act
+  const count = div.firstChild.querySelector('div');
+
+  // Assert
+  expect(count.textContent).toBe('0');
+
+  // Cleanup
+  div.remove();
+});
+
+test('increment counter', function () {
+  // Arrange
+  const div = document.createElement('div');
+  document.body.append(div);
+  render(<Counter />, div);
+
+  const increment = document.querySelector('button.increment');
+
+  // Act
+  increment.click();
+
+  console.log(document.body.innerHTML);
+  // Assert
+  const count = div.firstChild.querySelector('div');
+  expect(count.textContent).toBe('1');
+
+  div.remove();
 });
