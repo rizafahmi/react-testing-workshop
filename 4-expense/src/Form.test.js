60a50ec054d86754280b49fbfcebf50e7e3e98b2
Riza Fahmi <rizafahmi@gmail.com>
2022-05-03 11:34:53 +0700

    Start testing form submission. WIP

--- 4-expense/src/Form.test.js
+++ 4-expense/src/Form.test.js
@@ -1,7 +1,32 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Form from './Form.js';
 
 test('submit the form calls onSubmit with amount, category and date', function () {
-  render(<Form />);
+  let submittedData;
+
+  function handleSubmit(data) {
+    submittedData = data;
+  }
+
+  render(<Form onSubmit={handleSubmit} />);
+  const amountField = screen.getByLabelText(/amount/i);
+  const categoryField = screen.getByLabelText(/category/i);
+  const dateField = screen.getByLabelText(/date/i);
+  const submitButton = screen.getByRole('button', {
+    name: /save expense/i,
+  });
+  const now = Date.now();
+  userEvent.type(amountField, '100000');
+  userEvent.selectOptions(categoryField, 'Grocery');
+  userEvent.type(dateField, now);
+  userEvent.click(submitButton);
+
+  expect(submittedData).toEqual({
+    amount: '100000',
+    category: 'Grocery',
+    date: now,
+  });
+
   screen.debug();
 });
