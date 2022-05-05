d8e1f641eecd79ae54ee758e749ee239641a8730
Riza Fahmi <rizafahmi@gmail.com>
2022-05-05 11:50:49 +0700

    Finish context testing

---
+++ 5-contextual-testing/src/BigButton.test.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import BigButton from './BigButton.js';
+import { ThemeProvider } from './ThemeProvider.js';
+
+test('render <BigButton />', function () {
+  render(
+    <ThemeProvider>
+      <BigButton />
+    </ThemeProvider>
+  );
+});
